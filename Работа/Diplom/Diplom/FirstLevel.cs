5692061629222cd6ba1136d04705e35d2a228ee0
sasha2567 <lisyanscky.sasha2567@yandex.ru>
2016-04-29 22:01:02 +0300

    Добавил поля в структуру первого уровня, протестировал написанное

--- Работа/Diplom/Diplom/FirstLevel.cs
+++ Работа/Diplom/Diplom/FirstLevel.cs
@@ -10,10 +10,40 @@ namespace Diplom
     {
         private List<int> batchesList;
         private List<List<int>> batches;
+        private List<int> tempBatchesList;
+        private List<List<int>> tempBatches;
+        private int countType;
+        private List<int> countClaims;
 
-        public bool GenereteSolution()
+        public FirstLevel(int count_type, List<int> count_claims)
+        {
+            this.countType = count_type;
+            this.countClaims = count_claims;
+        }
+
+        public bool GenerateSolution()
         {
             return false;
+        }
+
+        public void GenerateStartSolution()
+        {
+            int m = 2;
+            int claim = 2;
+            this.batchesList = new List<int>(this.countType);
+            this.batches = new List<List<int>>(this.countType);
+            for (int i = 0; i < this.countType; i++)
+            {
+                this.batchesList.Add(m);
+                this.batches.Add(new List<int>());
+                this.batches[i].Add(this.countClaims[i] - claim);
+                this.batches[i].Add(claim);
+            }
+        }
+
+        public int GetCriterion()
+        {
+            return 0;
         }
     }
 }
--- Работа/Diplom/Diplom/Form1.cs
+++ Работа/Diplom/Diplom/Form1.cs
@@ -12,23 +12,36 @@ namespace Diplom
 {
     public partial class Form1 : Form
     {
+        private List<Batch> temp;
+        private BatchesGroup group;
+        private FirstLevel firstLevel;
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
             Batch x1 = new Batch(10, 1);
             Batch x2 = new Batch(2, 1);
             Batch x3 = new Batch(2, 1);
             Batch x4 = new Batch(2, 1);
-            List<Batch> temp = new List<Batch>();
+
+            temp = new List<Batch>();
             temp.Add(x1);
             temp.Add(x2);
             temp.Add(x3);
             temp.Add(x4);
-            BatchesGroup group = new BatchesGroup(temp);
+            group = new BatchesGroup(temp);
+            List<int> tmp = new List<int>();
+            tmp.Add(16);
+            tmp.Add(16);
+            tmp.Add(16);
+            tmp.Add(16);
+            firstLevel = new FirstLevel(4,tmp);
+            firstLevel.GenerateStartSolution();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
             group.modificationBatches(3,0,2);
             MessageBox.Show(group.printList());
         }
